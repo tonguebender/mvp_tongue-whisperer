bf5497c730620e6cd90e19e0a30dba9596fb5c91
Sergey Maksimov <smaksimoff@gmail.com>
2018-02-05 21:27:23 +0100

    bot api

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('init');
+if (!process.env.TONGUOLOGY_URL) {
+  process.env.TONGUOLOGY_URL = 'http://localhost:9091';
+}
+
+const botAPI = require('./lib/bot-api');
+const messages = require('./lib/messages');
+
+const bot = botAPI.start({ token: process.env.BOT_TOKEN });
+
+messages.start(bot);
