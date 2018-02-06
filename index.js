3c186f6cbb2c07584fc163ab78c8f06e85300f92
Sergey Maksimov <smaksimoff@gmail.com>
2018-02-06 14:56:22 +0100

    rename module

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-if (!process.env.TONGUOLOGY_URL) {
-  process.env.TONGUOLOGY_URL = 'http://localhost:9091';
-}
-
-const botAPI = require('./lib/bot-api');
+const telegramBotAPI = require('./lib/telegram-bot-api');
 const messages = require('./lib/messages');
 
-const bot = botAPI.start({ token: process.env.BOT_TOKEN });
+const bot = telegramBotAPI.start({ token: process.env.BOT_TOKEN });
 
 messages.start(bot);
